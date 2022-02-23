8a0ea1144b35431205f9924b7ceeb9c534b447ed
Kosteksal <kosteks@outlook.com>
2022-02-23 13:01:08 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    const arr = Array.from(String(n), Number);
+    const res = []
+    arr.forEach(function (i) {
+    if (isNaN(i) == false) {
+        res.push(i)
+    }
+   })
+
+   res2 = res.reverse().join('')
+
+   return res2
 }
